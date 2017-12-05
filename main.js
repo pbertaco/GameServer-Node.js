996375b2775bf9d6b52083901a38e3b174fbfaee
Pablo Henrique Bertaco <pbertaco@umov.me>
2017-12-05 13:46:13 -0200

    Translated comments to english Commented console.log calls Updated license

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ var app = require('http').createServer();
 app.listen(8900);
 
 function Game() {
-    console.log('Game()');
+    //console.log('Game()');
     this.io = require('socket.io')(app);
 
-    // Define a referência da lista de todos os Sockets conectados. 
-    // Esta lista é atualizada automaticamente pelo socket.io.
+    // Defines the list reference of all connected Sockets.
+    // This list is updated automatically by socket.io.
     this.connectedSockets = this.io.sockets.connected;
 
-    // Define a referência da lista de todas as salas ativas.
-    // Esta lista é atualizada automaticamente pelo socket.io
+    // Defines the list reference of all active rooms.
+    // This list is updated automatically by socket.io
     this.allRooms = this.io.sockets.adapter.rooms;
 
     this.addHandlers();
@@ -20,82 +20,82 @@ function Game() {
 
 function Player(game, socket) {
 
-    // Evita retenção do Player dentro dos handlers.
+    // Prevents player retention within handlers.
     var player = this;
 
-    // Definindo referências do Socket e Game para serem acessadas pelo Player.
+    // Defining Socket and Game references to be accessed by the Player.
     this.socket = socket;
     this.game = game;
 
-    // Aqui são definidos os comportamentos de todos os handlers.
-    // Não devem ser atribuidas muitas linhas de código para cada handler 
-    // pois estas linhas são alocadas novamente para cada Socket conectado.
+    // Here the behaviors of all the handlers are defined.
+    // Do not assign too many lines of code to each handler
+    // because these lines are allocated again for each connected Socket.
 
-    // Cada socket.on é executado quando o Game recebe um evento do Socket.
+    // Each socket.on is executed when the Game receives a Socket event.
 
     this.socket.on('createRoom', function () {
-        console.log(socket.name + ' on createRoom ');
+        //console.log(socket.name + ' on createRoom ');
         player.createRoom();
     });
 
     this.socket.on('userDisplayInfo', function (userDisplayInfo) {
 
-        // Definindo um nome para o Socket, somente para debug.
+        // Defining a name for the Socket, only for debugging.
         socket.name = userDisplayInfo;
 
-        console.log(socket.name + ' on userDisplayInfo ');
+        //console.log(socket.name + ' on userDisplayInfo ');
         player.setUserDisplayInfo(userDisplayInfo);
     });
 
     this.socket.on('userInfo', function (userInfo) {
-        console.log(socket.name + ' on userInfo ');
+        //console.log(socket.name + ' on userInfo ');
         player.setUserInfo(userInfo);
     });
 
     this.socket.on('update', function (data) {
-        console.log(socket.name + ' on update ');
+        //console.log(socket.name + ' on update ');
         player.update(data);
     });
 
     this.socket.on('someData', function (data) {
-        console.log(socket.name + ' on someData ');
+        //console.log(socket.name + ' on someData ');
         player.someData(data);
     });
 
     this.socket.on('leaveAllRooms', function () {
-        console.log(socket.name + ' on leaveAllRooms ');
+        //console.log(socket.name + ' on leaveAllRooms ');
         player.leaveAllRooms();
     });
 
     this.socket.on('getAllRooms', function () {
-        console.log(socket.name + ' on getAllRooms ');
+        //console.log(socket.name + ' on getAllRooms ');
         player.getAllRooms();
     });
 
     this.socket.on('getRoomInfo', function (roomId) {
-        console.log(socket.name + ' on getRoomInfo ');
+        //console.log(socket.name + ' on getRoomInfo ');
         player.getRoomInfo(roomId);
     });
 
     this.socket.on('joinRoom', function (roomId) {
-        console.log(socket.name + ' on joinRoom ');
+        //console.log(socket.name + ' on joinRoom ');
         player.joinRoom(roomId);
     });
 
     this.socket.on('disconnect', function () {
-        console.log(socket.name + ' on disconnect ');
+        //console.log(socket.name + ' on disconnect ');
         player.disconnect();
     });
 
-    // Foram definidos todos os handlers.
-    // Aqui o Player não precisa fazer mais nada alem disso.
+    // All handlers defined.
+    // Here the Player does not have to do anything else besides that.
 }
 
 // Implemantação das funções do Player.
 
 Player.prototype.leaveAllRooms = function () {
 
-    // Sai de todas as salas em que estiver dentro
+    // Leave all the rooms you are in
     for (var roomId in this.socket.adapter.sids[this.socket.id]) {
         this.leaveRoom(roomId);
     }
@@ -103,58 +103,58 @@ Player.prototype.leaveAllRooms = function () {
 
 Player.prototype.leaveRoom = function (roomId) {
 
-    // Avisa aos Sockets da sala que está saindo.
+    // Tell the Sockets in the room that they are leaving.
     this.socket.broadcast.to(roomId).emit('removePlayer', this.socket.id);
-    console.log(this.socket.name + ' broadcast emit removePlayer ');
+    //console.log(this.socket.name + ' broadcast emit removePlayer ');
 
-    // Saindo da sala
+    // Leaving the room
     this.socket.leave(roomId);
 };
 
 Player.prototype.createRoom = function () {
 
-    // Criando e entrando em uma sala.
+    // Creating and entering a room.
     this.socket.join(this.socket.id);
 
-    //Definindo roomId no Socket para marcar a sala criada.
+    // Defining roomId on the Socket to mark the created room.
     this.socket.roomId = this.socket.id;
 
-    // Respondendo ao Socket para que ele saiba o seu id.
+    // Replying to Socket so that it knows its id.
     this.socket.emit('mySocketId', this.socket.id);
-    console.log(this.socket.name + ' emit mySocketId ');
+    //console.log(this.socket.name + ' emit mySocketId ');
 };
 
 Player.prototype.setUserDisplayInfo = function (userDisplayInfo) {
 
-    // Definindo userDisplayInfo para manter informaçõees basicas do player
-    // de forma que possam ser acessadas diretamente pelo Socket.
+    // Setting userDisplayInfo to keep basic player information
+    // so that they can be accessed directly by the Socket.
     this.socket.userDisplayInfo = [this.socket.id, userDisplayInfo];
 };
 
 Player.prototype.setUserInfo = function (userInfo) {
 
-    // Definindo userInfo para manter todas as informacoes do player
-    // de forma que possam ser acessadas diretamente pelo Socket.
+    // Defining userInfo to keep all player information
+    // so that they can be accessed directly by the Socket.
     this.socket.userInfo = userInfo;
 };
 
 Player.prototype.update = function (data) {
     for (var roomId in this.socket.adapter.sids[this.socket.id]) {
         this.socket.broadcast.to(roomId).emit('update', data);
-        console.log(this.socket.name + ' broadcast emit update ');
+        //console.log(this.socket.name + ' broadcast emit update ');
     }
 };
 
 Player.prototype.someData = function (data) {
     for (var roomId in this.socket.adapter.sids[this.socket.id]) {
         this.socket.broadcast.to(roomId).emit('someData', data);
-        console.log(this.socket.name + ' broadcast emit someData ');
+        //console.log(this.socket.name + ' broadcast emit someData ');
     }
 };
 
 Player.prototype.getAllRooms = function () {
 
-    // Buscando informações de todas as salas do Game.
+    // Seeking information from all rooms of the Game.
     for (var roomId in this.game.allRooms) {
         this.getRoomInfo(roomId);
     }
@@ -170,24 +170,24 @@ Player.prototype.getRoomInfo = function (roomId) {
         roomInfo.usersDisplayInfo.push(someSocket.userDisplayInfo);
     }
 
-    // Respondendo ao Socket com a informação de uma sala e Sockets da sala. 
+    // Responding to the Socket with the information of a room and Sockets of the room. 
     this.socket.emit('roomInfo', roomInfo);
-    console.log(this.socket.name + ' emit roomInfo ');
+    //console.log(this.socket.name + ' emit roomInfo ');
 };
 
 Player.prototype.joinRoom = function (roomId) {
 
-    // Sai de todas as salas antes de entrar.
+    // Leave all rooms before entering.
     this.leaveAllRooms();
 
-    // Avisa aos Sockets da sala que esta entrando e envia suas informações básicas.
+    // Tell Sockets in the room that you are entering and send basic information.
     this.socket.broadcast.to(roomId).emit('addPlayer', this.socket.userDisplayInfo);
-    console.log(this.socket.name + ' broadcast emit addPlayer ');
+    //console.log(this.socket.name + ' broadcast emit addPlayer ');
 
-    // Entrando na sala
+    // Entering the room
     this.socket.join(roomId);
 
-    // Definindo id da sala atual.
+    // Defining id of the current room.
     this.socket.roomId = roomId;
 
     this.getRoomInfo(roomId);
@@ -195,28 +195,28 @@ Player.prototype.joinRoom = function (roomId) {
 
 Player.prototype.disconnect = function () {
 
-    //Executa leaveRoom ao ser desconectado para avisar aos outros Sockets que foi desconectado.
+    // Performs leaveRoom when disconnected to notify the other Sockets that it has been disconnected.
     this.leaveRoom(this.socket.roomId);
 };
 
 // Fim da implementacao das funções do Player.
 
 
-// Adicionando handlers ao Game
+// Adding handlers to the game
 Game.prototype.addHandlers = function () {
-    console.log('setHandlers()');
+    //console.log('setHandlers()');
 
-    // Evita retenção do Game dentro dos handlers.
+    // Avoid game retention within handlers.
     var game = this;
 
-    // Todo evento connection passa por aqui.
-    // Depois disso o tratamento dos outros eventos é definido pelo Player.
+    // Every connection event goes through here.
+    // After that, the treatment of the other events is defined by the Player.
     this.io.sockets.on("connection", function (socket) {
 
-        // Define um nome ao Socket, somente para debug.
+        // Defines a name for the Socket, only for debugging.
         socket.name = socket.id;
 
-        console.log(socket.name + ' on connection ');
+        //console.log(socket.name + ' on connection ');
         new Player(game, socket);
     });
 };
